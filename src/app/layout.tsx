951071b4d6a7ed70296c5bc503a8359120164ecf
henry-wu-1130 <henli.wu1491@gmail.com>
2025-06-24 17:13:07 +0800

    chore: update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import LanguageDetector from "@/components/LanguageDetector";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LanguageDetector />
         {children}
       </body>
     </html>
